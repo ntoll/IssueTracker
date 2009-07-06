87ddaec197341c5ce498c18d6e7db762e7135b3e
Nicholas Tollervey <ntoll@ntoll.org>
2009-07-06 13:08:07 +0100

    Generated initial structure of the application and modified settings.py to reflect these changes

--- settings.py
+++ settings.py
@@ -3,14 +3,17 @@
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+import os
+HOME = os.path.dirname(__file__)
+
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+     ('Nicholas Tollervey', 'ntoll@ntoll.org'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'issue_tracker'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -21,11 +24,11 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 SITE_ID = 1
 
@@ -35,7 +38,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = HOME + '/static'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -69,6 +72,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    HOME + '/templates',
 )
 
 INSTALLED_APPS = (
@@ -76,4 +80,29 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'workflow',
+    'project',
+    'tracker',
 )
+
+# Path to graphviz's dot command
+GRAPHVIZ_DOT_COMMAND = 'dot'
+
+DEFAULT_CHARSET = 'utf-8'
+
+# The default path (in URL) for the login page
+LOGIN_URL = '/login'
+# Where a successful login should redirect should a "next" argument not be
+# supplied in the URI
+LOGIN_REDIRECT_URL = '/'
+
+# Registration defaults:
+REGISTRATION_USE_HTTPS = True
+REGISTRATION_EMAIL_TEMPLATE = 'register_email.html'
+
+# Including a settings_local allows you to override these settings with
+# something more appropriate for your machine
+try:
+        from settings_local import *
+except:
+        pass
